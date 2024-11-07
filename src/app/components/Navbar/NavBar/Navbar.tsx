5cd55219edb77a9bf3600fca1aa9589b89f3a86e
Breno <bgferreira44@gmail.com>
2024-11-06 22:29:17 -0300

    fixed: add cn

--- src/app/components/Navbar/NavBar/Navbar.tsx
+++ src/app/components/Navbar/NavBar/Navbar.tsx
@@ -4,29 +4,34 @@ import {
   FaceHappyIcon,
   RouteIcon,
 } from "@/app/components/icons";
+import { cn } from "@/helpers/cn";
+import React from "react";
 
-const NavBarList = ({ children, className }: { children: React.ReactNode, className?: string }) => {
+type NavBarProps = React.ComponentProps<"nav">;
+type NavBarListProps = React.ComponentProps<"ul">;
+type NavBarListItemProps = React.ComponentProps<"li">;
+
+const NavBarList = ({ children, className, ...props }:  NavBarListProps) => {
   return (
-    <ul className={`my-4 border-t border-indigo-400/20 hover:border-indigo-400/40 ${className}`}>
+    <ul className={cn("my-4 border-t border-indigo-400/20 hover:border-indigo-400/40",  `${className}`)} {...props}>
       {children}
     </ul>
   );
 };
-const NavBarListItem = ({ children, className  }: { children: React.ReactNode , className?: string }) => {
+const NavBarListItem = ({ children, className, ...props }: NavBarListItemProps) => {
   return (
-    <li className={`my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 flex gap-2 items-center cursor-pointer hover:text-slate-100 ${className}`}>
+    <li className={cn("my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 flex gap-2 items-center cursor-pointer hover:text-slate-100",  `${className}`)} {...props}>
       {children}
     </li>
   );
 };
-//flex-grow border-t border-indigo-400/20 hover:border-indigo-400/40
-export const NavBar = () => {
+
+export const NavBar = ({ className, ...props}: NavBarProps) => {
   return (
-    <nav className="flex flex-col bg-slate-900 border-r border-indigo-400/20 h-screen hover:border-indigo-400/20 w-72 p-2 text-slate-300">
+    <nav className={cn("flex flex-col bg-slate-900 border-r border-indigo-400/20 h-screen hover:border-indigo-400/20 w-72 p-2 text-slate-300",  `${className}`)} {...props}>
       <div className="flex my-4 justify-center items-center w-full">
         <img src="/assets/logo.png" alt="logo breno" className="w-auto h-36" />
       </div>
-
         <NavBarList className="flex-grow">
             <NavBarListItem>
                 <HomeIcon className="w-4 h-4" />Home
