4d21504fe24459e6ba81874070d80748ce48d0fc
Breno <bgferreira44@gmail.com>
2024-11-12 20:32:58 -0300

    config

--- src/components/Navbar/NavBar/Navbar.tsx
+++ src/components/Navbar/NavBar/Navbar.tsx
@@ -10,13 +10,14 @@ import { NavBarProps } from "./types";
 import { NavBarList } from "./NavBarList";
 import { NavBarListItemLink } from "./NavBarListItemLink";
 import { NavBarListItemButton } from "./NavBarListItemButton";
+import Image from "next/image";
 
 
 export const NavBar = ({ className, ...props}: NavBarProps) => {
   return (
     <nav className={cn("flex flex-col bg-slate-900 border-r border-indigo-400/20 h-screen hover:border-indigo-400/20 w-72 p-2 text-slate-300",  `${className}`)} {...props}>
       <div className="flex my-4 justify-center items-center w-full">
-        <img src="/assets/logo.png" alt="logo breno" className="w-auto h-36" />
+        <Image src="/assets/logo.png" alt="logo breno" className="w-auto h-36" width={120} height={32}/>
       </div>
         <NavBarList className="flex-grow">
             <NavBarListItemLink href={"/"}>
