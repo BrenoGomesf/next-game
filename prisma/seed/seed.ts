5bcd75711279b528bd181688b996cae60fc77739
Breno <bgferreira44@gmail.com>
2024-12-26 21:51:23 -0300

    feat: import articles data from json file

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,36 @@
 import { PrismaClient } from "@prisma/client";
 
+import Articles  from "../../src/data/articles.json"
+import { slugfy } from "../../src/helpers/slugfy";
 const prisma = new PrismaClient();
 
+const isDev = process.env.NODE_ENV == "development";
 async function main() {
-console.log("db seed")
+  await seedArticles();
+  console.log("db seed")
+}
+
+async function  seedArticles(){
+  if(!isDev) throw new Error('NODE_ENV is not a development enviroment.');
+
+  //Delete all
+  await prisma.article.deleteMany();
+  //trucante
+  await prisma.$executeRawUnsafe(`DELETE FROM SQLITE_SEQUENCE WHERE name=$1`, "Article");
+
+  for(let article of Articles){
+    const record = await prisma.article.create({
+      data: {
+        title:        article.title,
+        slug:         slugfy(article.title),
+        excerpt:      article.excerpt,
+        content:      article.content,
+        image:        article.image,
+        publishedAt:  new Date(article.publish_date),
+        }
+      });
+      console.log("*** create article***", record.id, record.title)
+    }
 }
 
 main().then(async () => {
---
+++ src/helpers/slugfy.ts
@@ -0,0 +1,7 @@
+import {convert} from 'url-slug';
+ 
+export const slugfy = (value: string) => {
+    return convert(value, {
+        dictionary: { "'":"" }
+    });
+};
