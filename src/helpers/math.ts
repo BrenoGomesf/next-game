3c7ef5ab24e8444fd706d95bd2f527693b693e14
Breno <bgferreira44@gmail.com>
2024-12-31 12:43:31 -0300

    fixed: Improvement on getting random

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -1,7 +1,21 @@
 export const getIntArray = (min: number, max: number) => {
-    const result: number[] = [];
-    for(let i = min; i <= max; i++){
-        result.push(i);
+  const result: number[] = [];
+  for (let i = min; i <= max; i++) {
+    result.push(i);
+  }
+  return result;
+};
+
+export const getRandomIntArrayInRange = (
+  min: number,
+  max: number,
+  count: number
+) => {
+    const range = Array.from({ length: max - min + 1 }, (_, idx) => min + idx);
+    for (let i = range.length - 1; i > 0; i--) {
+      const randomIdx = Math.floor(Math.random() * (i + 1));
+      [range[i], range[randomIdx]] = [range[randomIdx], range[i]];
     }
-    return result
-}
+    
+    return range.slice(0, count);
+};
--- src/services/Games.ts
+++ src/services/Games.ts
@@ -1,14 +1,15 @@
+import { getRandomIntArrayInRange } from "@/helpers/math";
 import { sleep } from "@/helpers/promises";
 import Games from "@/libs/database/Games";
 
 
 const GamesService = {
     getGamesBySlug: async (slug: string) => {
-        await sleep(1000);
+        //await sleep(1000);
         return Games.getOne({ where: {slug}})
     },
     getGamesList: async (page = 1, limit = 10) => {
-        await sleep(1000);
+        //await sleep(1000);
         const offset  = (page - 1) * limit;
         const data    = await Games.get({}, {}, limit, offset);
         const total    = await  Games.count({});
@@ -28,8 +29,10 @@ const GamesService = {
     getRandomGames: async(limit = 10) => {
         const total = await  Games.count({});
         const offset = Math.max(0, Math.floor(Math.random() * Number(total)) - limit);
-        const data = await Games.get({},{}, limit, offset);
-        const sorted = data.sort(() =>Math.random() > 0.5 ? 1 : -1);
+        const ids = getRandomIntArrayInRange(0, total, limit);
+     
+        const where = {id: {in: ids}}
+        const data = await Games.get(where,{}, limit);
         const totalPages  = Math.ceil(total / limit);
 
         return {
